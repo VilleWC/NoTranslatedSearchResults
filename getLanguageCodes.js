42ed90ea3f5a9d828dc268f4767fc7a0ccda7f69
Ville Wennerlund <villeloveandre@gmail.com>
2025-03-10 22:19:11 +0100

    initial commit

---
+++ getLanguageCodes.js
@@ -0,0 +1,12 @@
+// const td = document.querySelectorAll("td")
+// const test = [...td].filter((x, i) => {
+//     console.log(i % 2)
+//     if(i % 2 === 0) {
+//         return false;
+//     }
+//     return true;
+// }).map(x => {
+//     return x.innerHTML;
+// })
+// console.log(test)
+
